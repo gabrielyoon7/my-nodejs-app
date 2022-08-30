1bb8415e146b33ecd39a46572ba8c57f0f591dd7
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-08-31 03:09:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// const arr = [1,2,3,4];
+// arr.forEach(console.log);
+
+console.log(score);
+score = 80;
+var score;
+console.log(score);
+
+var wow;
+console.log(wow??"yeah")
+
+const person = {
+    name:'Gabriel',
+    sayHello:function(){
+        console.log('hihi');
+    }
+}
+console.log(typeof person)
+console.log(person)
+
+const empty={}
+console.log(typeof empty)
+console.log(empty)
