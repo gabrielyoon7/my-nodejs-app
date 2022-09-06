6f6814e0beebf57ea05b4e480ff9a8cf699adb3c
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-09-06 15:50:30 +0900

    .

--- index.js
+++ index.js
@@ -168,18 +168,34 @@
 // console.log(descriptor);
 //   // {get: ƒ, set: ƒ, enumerable: true, configurable: true}
 
-// 생성자 함수
-function Circle(radius) {
-    // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
-    this.radius = radius;
-    this.getDiameter = function () {
-      return 2 * this.radius;
-    };
-  }
-  
-  // 인스턴스의 생성
-  const circle1 = new Circle(5);  // 반지름이 5인 Circle 객체를 생성
-  const circle2 = new Circle(10); // 반지름이 10인 Circle 객체를 생성
-  
-  console.log(circle1.getDiameter()); // 10
-  console.log(circle2.getDiameter()); // 20
+// // 생성자 함수
+// function Circle(radius) {
+//     // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+//     this.radius = radius;
+//     this.getDiameter = function () {
+//         return 2 * this.radius;
+//     };
+// }
+
+// // 인스턴스의 생성
+// const circle1 = new Circle(5);  // 반지름이 5인 Circle 객체를 생성
+// const circle2 = new Circle(10); // 반지름이 10인 Circle 객체를 생성
+
+// console.log(circle1.getDiameter()); // 10
+// console.log(circle2.getDiameter()); // 20
+
+// 함수는 다양한 방식으로 호출될 수 있다.
+function foo() {
+    console.log(this);
+}
+
+// 일반적인 함수로서 호출
+// 전역 객체는 브라우저 환경에서는 window, Node.js 환경에서는 global을 가리킨다.
+foo(); // window
+
+// 메서드로서 호출
+const obj = { foo }; // ES6 프로퍼티 축약 표현
+obj.foo(); // obj
+
+// 생성자 함수로서 호출
+const inst = new foo(); // inst
