f0c60c649856c6f6219e9f0ff7d163c5c6f8b731
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-01 00:14:33 +0900

    refactor: attribute를 customElements에 넘기는 실습

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 console.log('실행');
 
-// class MyElement extends HTMLElement {
-//   constructor() {
-//     super();
-//     this.innerHTML = 'Hello, world!';
-//   }
-//   connectedCallback() {
-//     console.log('MyElement was inserted into the document');
-//   }
-
-//   static get observedAttributes() {
-//     return ['my-attribute'];
-//   }
-
-//   attributeChangedCallback(name, oldValue, newValue) {
-//     console.log(`Attribute ${name} changed from ${oldValue} to ${newValue}`);
-//   }
-// }
-// customElements.define('my-element', MyElement);
-
-
 class MyElement extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `
-      <div>
-        <slot></slot>
-      </div>
-    `;
+    const myAttribute = this.getAttribute('my-attribute');
+    console.log(myAttribute); // prints the value of the 'my-attribute' attribute
+    this.innerHTML = 'Hello, world!';
+  }
+  connectedCallback() {
+    console.log('MyElement was inserted into the document');
+  }
+
+  static get observedAttributes() {
+    return ['my-attribute'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute ${name} changed from ${oldValue} to ${newValue}`);
   }
 }
-
 customElements.define('my-element', MyElement);
+
+
+// class MyElement extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.attachShadow({ mode: 'open' });
+//     this.shadowRoot.innerHTML = `
+//       <div>
+//         <slot></slot>
+//       </div>
+//     `;
+//   }
+// }
+
+// customElements.define('my-element', MyElement);
