8a3dbd3efce34d32bcf8b6b9d84017ce21751806
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-03 14:28:22 +0900

    refactor: 주석 처리 복구

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log('실행');
 customElements.define('my-element', MyElement);
 
 // https://itnext.io/adding-state-to-custom-html-elements-639961c7c529
-// customElements.define('my-button', MyButton);
+customElements.define('my-button', MyButton);
 
 customElements.define('my-counter', MyCounter);
 customElements.define('my-array', MyArray);
