1fb606b9bf970b8f77be5f5735c907b666d9cdc7
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-12 18:20:08 +0900

    refactor: 객체 배열

---
+++ src/objects/object-array.js
@@ -0,0 +1,19 @@
+const players = [
+  {
+    name: '메시',
+    age: 36
+  },
+  {
+    name: '호날두',
+    age: 38
+  },
+  {
+    name: '손흥민',
+    age: 31
+  },
+  {
+    name: '이강인',
+    age: 23
+  },
+]
+players.forEach((p) => console.log(p.name))
