232667105d116f809a7e29eabbad0b59ad2dbd13
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-18 01:55:56 +0900

    refactor: .then()의 다양한 사용법

--- src/fooooooo.js
+++ src/fooooooo.js
@@ -15,3 +15,13 @@ Promise.race([
   }).catch(() => {
     console.error("대기 시간이 오래 걸립니다");
   });
+
+
+const getPopularMovies2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, Math.random() * 4000 + 1000, "외부 API 접근한 결괏값");
+  setTimeout(reject, 3000, "대기 시간이 오래 걸립니다");
+});
+getPopularMovies2.then(
+  (result) => console.log(result),
+  (error) => console.error(error)
+);
