01abdb7a900cbde1006fbc4e9538b99d00026b56
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-28 23:56:20 +0900

    refactor: 미사용 함수 제거

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 /* eslint-disable import/extensions */
 import MyButton from './src/customElements/MyButton.js';
 import MyElement from './src/customElements/MyElement.js';
-import MyProxyElement from './src/customElements/MyProxyElement.js';
 import ObjButton1 from './src/customElements/ObjButton1.js';
 import ObjButton2 from './src/customElements/ObjButton2.js';
 import MyCustomElement from './src/customElements/ObserverProxyPattern.js';
 import MyEnhancedElement from './src/customElements/MyEnhancedElement.js';
-import MyCustomEvenetElement from './src/customElements/MyCustomEvenetElement.js';
 
 console.log('실행');
 
@@ -15,26 +13,8 @@ customElements.define('my-element', MyElement);
 // https://itnext.io/adding-state-to-custom-html-elements-639961c7c529
 customElements.define('my-button', MyButton);
 
-customElements.define('my-proxy', MyProxyElement);
-
 customElements.define('obj-button1', ObjButton1);
 customElements.define('obj-button2', ObjButton2);
 
 
-// Register the custom element
-customElements.define('my-custom-element', MyCustomElement);
-
-// Usage
-const customElement = document.createElement('my-custom-element');
-document.body.appendChild(customElement);
-customElement.state.title = 'Hello, World!';
-customElement.state.content = 'This is a custom element';
-
-
-
 customElements.define('my-enhanced-element', MyEnhancedElement);
-
-
-
-
-customElements.define('my-custom-event-element', MyCustomEvenetElement);
--- src/customElements/MyCustomEvenetElement.js
+++
@@ -1,8 +0,0 @@
-export default class MyCustomEvenetElement extends HTMLElement {
-  connectedCallback() {
-    this.addEventListener('myCustomEvent', (event) => {
-      console.log(`myCustomEvent : ${event.detail.key}`);
-    });
-    this.innerHTML = `--MyCustomEvenetElement--`;
-  }
-}
