b98d90134f4d46324d9c2feb36ccac0ae94bae8a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-04-08 17:24:56 +0900

    refactor: 클로저를 활용하여 함수 밖 변수 제거

--- src/html-template/index.js
+++ src/html-template/index.js
@@ -1,13 +1,15 @@
 console.log('import 성공!');
 const template = document.getElementById('template');
 const container = document.getElementById('list');
-let count = 0;
 
 function addItem() {
-  const newComponent = template.content.cloneNode(true);
-  newComponent.querySelector('.component').id = `item${count++}`;
-  newComponent.querySelector('.text').innerText = `아이템 ${count}`;
-  container.appendChild(newComponent);
+  let count = 0;
+  return () => {
+    const newComponent = template.content.cloneNode(true);
+    newComponent.querySelector('.component').id = `item${count++}`;
+    newComponent.querySelector('.text').innerText = `아이템 ${count}`;
+    container.appendChild(newComponent);
+  }
 }
 
 function deleteItem(button) {
@@ -15,7 +17,7 @@ function deleteItem(button) {
 }
 
 const addButton = document.getElementById('add-button');
-addButton.addEventListener('click', addItem);
+addButton.addEventListener('click', addItem());
 
 // 이벤트 버블링으로 리스너 수를 줄임
 container.addEventListener('click', event => {
